77cb06ebd0e57239d9c5505e9fd6fce0d0d260ea
strayer_practice@outlook.com <strayer_practice@outlook.com>
2022-10-27 16:38:00 -0700

    course_project_phase1_final

--- CourseProjectPhase1_CreateAndCallFunctionsWPar.py
+++ CourseProjectPhase1_CreateAndCallFunctionsWPar.py
@@ -25,6 +25,27 @@ def PrintTotals(TotEmployees, TotHours, TotGrossPay, TotTax, TotNetPay):
     print()
     print(f"Total Number Of Employees: {TotEmployees}")
 def printinfo(TotHours, TotGrossPay, TotTax, TotNetPay):
-    print(TotHours, f"{TotHours:,.2f}", f"{TotGrossPay:,.2f}", f"{TotTax:,.2f}, f"
+    print(TotHours, f"{TotHours:,.2f}", f"{TotGrossPay:,.2f}", f"{TotTax:,.2f}", f"{TotNetPay:,.2f}")
+
+if __name__ == "__main__":
+    TotEmploees = 0
+    TotHours = 0.00
+    TotGrossPay = 0.00
+    TotTax = 0.00
+    TotNetPay = 0.00
+    while True:
+        empname = GetEmpName()
+        if (empname.upper() == "END"):
+            break
+        hours = GetHoursWorked()
+        if (hours.upper() == "END"):
+            break
+        hourlyrate = GetHourlyRate()
+        if (hourlyrate.upper() == "END"):
+            break
+        taxrate = GetTaxRate()
+        if (taxrate.upper() == "END"):
+            break
+
 
 
